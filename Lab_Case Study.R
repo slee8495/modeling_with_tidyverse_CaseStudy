1a03c66e4a57b45797875e6e6bda0b59acadb440
S Lee <sanlee8495@gmail.com>
2022-07-09 13:37:49 -0700

    Splitting Data
    7/9/22

--- Lab_Case Study.R
+++ Lab_Case Study.R
@@ -1,6 +1,7 @@
 # Case Study #1: Predicting Annual Air  Pollution
 
 library(tidyverse)
+library(tidymodels)
 library(magrittr)
 library(skimr)
 
@@ -18,3 +19,12 @@ pm %>%
   dplyr::select_if(is.numeric) %>% cor() -> pm_cor
 
 corrplot::corrplot(pm_cor, tl.cex = 0.5)
+
+# Splitting Data
+set.seed(1234)
+pm_split <- rsample::initial_split(data = pm, prop = 2/3)
+
+train_pm <- rsample::training(pm_split)
+test_pm <- rsample::testing(pm_split)
+
+# Making a Receipt
